a4533498d47ab17d6b0d237abcd22c0300be7ba2
rcarrasco03652 <rcarrasco0@ucol.mx>
2019-11-15 18:47:10 -0600

    Terminado

--- Arbol.js
+++ Arbol.js
@@ -4,6 +4,9 @@ export default class Arbol{
     constructor(){
         this._raiz = null
     }
+    get raiz(){
+        return this._raiz;
+    }
 
     agregar(articulo){
         let newArticulo = new Articulo(articulo);
@@ -13,21 +16,50 @@ export default class Arbol{
         }else{
             this._agregarArticulo(this._raiz, newArticulo)
         }
+
+        console.log(this._raiz);
     }
 
     _agregarArticulo(raiz, newArticulo){
-        if(newArticulo.codigo < raiz.codigo){
+        if(raiz.codigo === newArticulo.codigo){
+            alert("Este producto ya existe")
+        }else if(newArticulo.codigo < raiz.codigo){
            if(raiz.izquierda===null){
             raiz.izquierda = newArticulo
            }else{
             this._agregarArticulo(raiz.izquierda, newArticulo)
            }
-        }else if(newArticulo.codigo > raiz.codigo){
+        }else{
             if(raiz.derecha===null){
                 raiz.derecha = newArticulo
             }else{
                 this._agregarArticulo(raiz.derecha, newArticulo)
             }
+        }
+    }
+
+    inOrder(articulo, div){
+        if(articulo !== null){
+            this.inOrder(articulo.izquierda, div)
+            div.innerHTML += articulo.codigo
+            this.inOrder(articulo.derecha, div)
+        }
+
+        
+    }
+
+    preOrder(articulo,div){
+        if(articulo!== null){
+            div.innerHTML += articulo.codigo
+            this.preOrder(articulo.izquierda, div)
+            this.preOrder(articulo.derecha, div)
+        }
+    }
+    postOrder(articulo,div){
+        if(articulo!== null){
+            this.postOrder(articulo.izquierda, div)
+            this.postOrder(articulo.derecha, div)
+            div.innerHTML += articulo.codigo
         }
     }
 }
--- main.js
+++ main.js
@@ -4,8 +4,27 @@ import Arbol from "./Arbol.js";
 class Main{
     constructor(){
         this._arbol = new Arbol();
-        
 
+        document.getElementById('agregar').addEventListener('click', () => {
+            let objArticulo = {
+                codigo: Number(document.getElementById('codigoArticulo').value),
+                nombre: document.getElementById('nombreArticulo').value,
+                precio: Number(document.getElementById('precioArticulo').value),
+                contenido: document.getElementById('contenidoArticulo').value,
+                descripcion: document.getElementById('descripcionArticulo').value
+            };
+
+            this._arbol.agregar(objArticulo);
+        });
+        document.getElementById('imprimir').addEventListener('click', () => {
+            let div = document.getElementById('print')
+            this._arbol.inOrder(this._arbol.raiz, div)
+            div.innerHTML += "<br>"
+            this._arbol.preOrder(this._arbol.raiz, div)
+            div.innerHTML += "<br>"
+            this._arbol.postOrder(this._arbol.raiz, div)
+        })
+        
     }
 }
 
